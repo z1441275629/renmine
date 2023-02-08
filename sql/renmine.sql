671f543a193e03aa864955fbb81816d42dbd1f42
zhaodsh <zhaodesheng@chemical.ai>
2023-02-08 23:44:58 +0800

    数据库备份

---
+++ sql/renmine.sql
@@ -0,0 +1,26 @@
+﻿# Host: localhost  (Version: 5.7.26)
+# Date: 2023-02-08 23:42:51
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `name` varchar(64) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "users"
+#
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'1441275629@qq.com','zds','123');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
