ab6d8c0740e3a28cbaa7a651d6f28a233f162b3f
zhaodsh <zhaodesheng@chemical.ai>
2023-02-10 01:12:34 +0800

    备份数据库

--- sql/renmine.sql
+++ sql/renmine.sql
@@ -1,8 +1,29 @@
 ﻿# Host: localhost  (Version: 5.7.26)
-# Date: 2023-02-08 23:42:51
+# Date: 2023-02-10 01:11:58
 # Generator: MySQL-Front 5.3  (Build 4.234)
 
 /*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "message"
+#
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentId` int(11) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `userId` int(11) NOT NULL DEFAULT '0',
+  `time` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "message"
+#
+
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
 
 #
 # Structure for table "users"
@@ -15,12 +36,12 @@ CREATE TABLE `users` (
   `name` varchar(64) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "users"
 #
 
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'1441275629@qq.com','zds','123');
+INSERT INTO `users` VALUES (1,'1441275629@qq.com','zds','123'),(2,'zhaodesheng@chemical.ai','zds2','666');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
